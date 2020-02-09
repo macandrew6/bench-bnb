dbba537b292d562584cd897ef379f7853dad1aca
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-08 16:41:19 -0800

    add jbuilder view for user

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,5 +4,7 @@ class Api::SessionsController < ApplicationController
 
   def destroy
     # eliminate session token
+    # Render an empty {} upon successful logout.
+    # Render a 404 message if there is no current_user to logout.
   end
 end
