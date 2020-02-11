22c7c7851af3bdff2a06c369dfd3a7db9e47a05f
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-10 16:21:07 -0800

    completes user migration updates schema

--- db/migrate/20200207010125_create_users.rb
+++ db/migrate/20200207010125_create_users.rb
@@ -4,8 +4,10 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :username, null: false
       t.string :password_digest, null: false
       t.string :session_token, null: false
-      
+
       t.timestamps
     end
+    add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
   end
 end
