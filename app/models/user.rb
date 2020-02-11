42e5b7a5832b2b6f5ae4795c8bc9ed24c27f83a1
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-10 16:10:15 -0800

    adds password fns

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,17 @@ class User < ApplicationRecord
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil unless user
-    # 
+    # helper method for user_password ? return user : nil
   end
+
+  def password=(password)
+    @password=password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  
 end
