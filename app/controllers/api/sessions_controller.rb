f709dd414b0c41574489ee4ca640844bfff5c748
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-11 18:38:27 -0800

    creates controller for session

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,16 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render json: ["Invalid username/password combination"], status: 401
+    end
   end
 
   def destroy
