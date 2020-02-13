18dc00f4321d73f5be7d6aca72c23b6696af3b44
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-12 23:22:34 -0800

    done testing session utils

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { login, signup, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
+  // window.login = login;
+  // window.signup = signup;
+  // window.logout = logout;
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
