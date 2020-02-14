d04c23310fc5ece9b66132b645e38c745b385cea
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-13 22:52:23 -0800

    removing test code

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { login, signup, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // window.login = login;
-  // window.signup = signup;
-  // window.logout = logout;
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
