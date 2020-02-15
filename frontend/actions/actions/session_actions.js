2682b1d5eaf67a139b7c3f5a1696d52aced57d08
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-14 21:41:01 -0800

    writes logout user thunk action creator

--- frontend/actions/actions/session_actions.js
+++ frontend/actions/actions/session_actions.js
@@ -18,7 +18,13 @@ export const receiveErrors = errors => ({
   errors
 });
 
-
+export const signup = user => dispatch => (
+  APIUtil.signup(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
 
 export const login = user => dispatch => (
   APIUtil.login(user).then(user => (
