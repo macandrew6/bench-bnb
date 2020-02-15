1c8b7d02ba2025686c95032074ec553d1b6ce329
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-14 21:30:17 -0800

    adds session actions

---
+++ frontend/actions/actions/session_actions.js
@@ -0,0 +1,17 @@
+import * as APIUtil from "../../util/session_api_util";
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+export const receiveCurrentUser = currentUser  => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+
+// export const login = user => dispatch => (
+//   APIUtil.login(user).then(user => (
+//     dispatch()
+//   ))
+// )
