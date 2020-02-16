60cd4997994f7c63306df60b7b52f2f69bf7ad7c
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-15 16:48:52 -0800

    adds greeting components

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/Root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   
+  //testing start
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  //testing end
+  
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
