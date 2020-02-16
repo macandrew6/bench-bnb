918c8ba7ed94d4e109b4de5f4c51b7acd6cd9d93
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-15 17:00:23 -0800

    testing greeting

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/Root';
+import { login, logout, signup } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   
   //testing start
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   //testing end
