20f1b8592805849ad58b99fa3872c9deaa640e96
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-17 14:50:05 -0800

    bootstraps user to the frontend

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -4,9 +4,24 @@ import configureStore from './store/store';
 import Root from './components/Root';
 import { login, logout, signup } from './actions/session_actions';
 
+
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+  let store;
   
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: {
+          [window.currentUser.id]: window.currentUser
+        }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   //testing start
   window.login = login;
   window.logout = logout;
