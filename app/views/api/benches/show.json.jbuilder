b7abded9da703ff97b7977509b0705bb3c161949
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-19 14:07:01 -0800

    adds seed file and finishes bench views

---
+++ app/views/api/benches/show.json.jbuilder
@@ -0,0 +1,3 @@
+json.bench do 
+  json.partial! 'bench', bench: @bench
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Bench.create!({
+  lat: -122.432813, 
+  lng: 37.795741, 
+  description: 'random beige house brown trim'
+})
+
+Bench.create!({
+  lat: -122.437784, 
+  lng: 37.790182, 
+  description: 'clay st house next to park'
+})
+
+Bench.create!({
+  lat: -122.451116, 
+  lng: 37.763076, 
+  description: 'house next to a school'
+})
