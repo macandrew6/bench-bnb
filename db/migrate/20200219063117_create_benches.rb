c55410235ec2563bb6a0e1f7be688625063a0d91
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-19 01:49:04 -0800

    migrates to schema bench props

--- db/migrate/20200219063117_create_benches.rb
+++ db/migrate/20200219063117_create_benches.rb
@@ -1,7 +1,10 @@
 class CreateBenches < ActiveRecord::Migration[5.2]
   def change
     create_table :benches do |t|
-
+      t.string :description
+      t.float :lng
+      t.float :lat
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_010125) do
+ActiveRecord::Schema.define(version: 2020_02_19_063117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "benches", force: :cascade do |t|
+    t.string "description"
+    t.float "lng"
+    t.float "lat"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
