f390269cb64598ef163e0fb0743318da61881956
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-19 13:26:32 -0800

    inits bench views

--- app/controllers/api/benches_controller.rb
+++ app/controllers/api/benches_controller.rb
@@ -2,11 +2,17 @@ class Api::BenchesController < ApplicationController
   before_action :require_logged_in, only: [:create]
   
   def create
-
+    @bench = Bench.create!(bench_params)
+    render :show
   end
   
   def index
-    
+    @benches = Bench.all
+    render :index
+  end
+
+  def show
+    @bench = Bench.find(params[:id])
   end
 
   private
