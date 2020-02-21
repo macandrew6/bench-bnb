49cb6f1081ddd66f55173de53d8512059ecbc9e2
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-20 19:17:00 -0800

    adds map component

---
+++ frontend/components/bench_map.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class BenchMap extends Component {
+  render() {
+    return (
+      <div id="map-container" ref='map'>
+        
+      </div>
+    );
+  }
+}
