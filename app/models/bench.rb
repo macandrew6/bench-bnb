c4855a99c0354bab23d151b281e510c32d6856a1
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-24 23:18:30 -0800

    adds bounds

--- app/models/bench.rb
+++ app/models/bench.rb
@@ -2,6 +2,9 @@ class Bench < ApplicationRecord
   validates :description, :lat, :lng, presence: true
 
   def self.in_bounds(bounds)
-
+    self.where('lat < ?', bounds[:northEast][:lat])
+      .where('lat > ?', bounds[:southWest][:lat])
+      .where('lng < ?', bounds[:northEast][:lng])
+      .where('lng > ?', bounds[:southWest][:lng])
   end
 end
