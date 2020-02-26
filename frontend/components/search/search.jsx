caf18f7d74e57ba9bf7df6dfec0a7287c03aa0b0
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-26 15:12:05 -0800

    refactors filters

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import BenchMap from '../bench_map/bench_map';
-import BenchIndex from './bench_index'
+import BenchIndex from './bench_index';
 
 const Search = ({ benches, fetchBenches, updateFilter }) => {
   return (
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,13 +3,13 @@ import { combineReducers } from 'redux';
 import entitiesReducer from './entities_reducer';
 import errorsReducer from './errors_reducer';
 import sessionReducer from './session_reducer';
-import filtersReducer from './filters_reducer';
+import uiReducer from './ui_reducer';
 
 const rootReducer = combineReducers({
   entities: entitiesReducer,
   session: sessionReducer,
   errors: errorsReducer,
-  filters: filtersReducer
+  ui: uiReducer
 });
 
 export default rootReducer;
