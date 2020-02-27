a28b2b881e7fd40988ba879e038238a0a6b04cde
macandrew6 <mr.betterandrewmac@gmail.com>
2020-02-26 18:59:40 -0800

    adds column to benches table

---
+++ db/migrate/20200227025514_adds_seating_to_bench_table.rb
@@ -0,0 +1,5 @@
+class AddsSeatingToBenchTable < ActiveRecord::Migration[5.2]
+  def change
+    add_column :benches, :seating, :integer, default: 2, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_063117) do
+ActiveRecord::Schema.define(version: 2020_02_27_025514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_02_19_063117) do
     t.float "lat"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "seating", default: 2, null: false
   end
 
   create_table "users", force: :cascade do |t|
