65d5249df71594d5cfdb4c98ba4a0bac519221dc
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-01 23:34:36 -0800

    page loses connection to the store when reloading

--- frontend/components/bench_show/bench_show.jsx
+++ frontend/components/bench_show/bench_show.jsx
@@ -3,6 +3,7 @@ import BenchMap from '../bench_map/bench_map';
 import { Link } from 'react-router-dom';
 
 // why when i refresh this page do i lose connection with the store
+// still not sure why this happens
 
 const BenchShow = ({ bench, fetchBench, benchId }) => {
   const benches = {
