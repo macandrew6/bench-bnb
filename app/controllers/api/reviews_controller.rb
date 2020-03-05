27855dd368d5eba973efad6001025a27fa4df73f
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-05 11:21:21 -0800

    adds views and j builder

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -2,6 +2,18 @@ class Api::ReviewsController < ApplicationController
   before_action :require_logged_in
   
   def create
-    
+    @review = current_user.reviews.new(review_params)
+
+    if @review
+      render :show
+    else
+      render json: @review, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:reviews).permit(:rating, :body, :bench_id)
   end
 end
