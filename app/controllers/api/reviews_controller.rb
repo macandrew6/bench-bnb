acae690a879b98dfd1c56aa82845a353bfdc140c
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-04 18:01:49 -0800

    sets up associations between reviews and users and reviews and benches

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,5 +1,7 @@
 class Api::ReviewsController < ApplicationController
+  before_action :require_logged_in
+  
   def create
-
+    
   end
 end
--- app/models/bench.rb
+++ app/models/bench.rb
@@ -14,6 +14,8 @@
 class Bench < ApplicationRecord
   validates :description, :lat, :lng, presence: true
 
+  has_many :reviews
+
   def self.in_bounds(bounds)
     self.where('lat < ?', bounds[:northEast][:lat])
       .where('lat > ?', bounds[:southWest][:lat])
