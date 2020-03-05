251caf691dca9f368f991033be8e027a3caa54ec
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-04 16:32:07 -0800

    creates migration for reviews with indexed on bench_id

--- db/migrate/20200304221041_create_reviews.rb
+++ db/migrate/20200304221041_create_reviews.rb
@@ -1,8 +1,12 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-
+      t.string :body, default: "", null: false
+      t.integer :rating, null: false
+      t.integer :bench_id, null: false
+      t.integer :author_id, null: false
       t.timestamps
     end
+    add_index :reviews, :bench_id
   end
 end
