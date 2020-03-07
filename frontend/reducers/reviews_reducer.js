8bbd9c06a50bbbd1579e26ad4e5f1616a56f1114
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-06 17:46:02 -0800

    adds reviews reducer

---
+++ frontend/reducers/reviews_reducer.js
@@ -0,0 +1,16 @@
+import { RECEIVE_REVIEW, RECEIVE_BENCH } from '../actions/bench_actions';
+
+const reviewsReducer = (state, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_BENCH:
+      return Object.assign({}, state, action.reviews);
+    case RECEIVE_REVIEW:
+      const { review } = action;
+      return Object.assign({}, state, { [review.id]: review });
+    default:
+      return state;
+  }
+};
+
+export default reviewsReducer;
