e382fbab0b080b340b14d118a0d4d24b0a259d55
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-17 15:16:28 -0700

    actual links

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,27 +29,50 @@ bench3 = Bench.create!({
   description: 'cool bench'
 })
 
+# POJO_IMGS = [
+#   'app/assets/images/pojo_img1.jpg',
+#   'app/assets/images/pojo_img2.jpg',
+#   'app/assets/images/pojo_img3.jpg',
+#   'app/assets/images/pojo_img4.jpg'
+# ]
+
 POJO_IMGS = [
-  'app/assets/images/pojo_img1.jpg',
-  'app/assets/images/pojo_img2.jpg',
-  'app/assets/images/pojo_img3.jpg',
-  'app/assets/images/pojo_img4.jpg'
+  "https://amac-bench-bnb-dev.s3-us-west-1.amazonaws.com/67gcsU2rQoZhUimQqVQrzqaV",
+  "https://amac-bench-bnb-dev.s3-us-west-1.amazonaws.com/7UYWzG3YEAmbc14uKHSXky1j",
+  "https://amac-bench-bnb-dev.s3-us-west-1.amazonaws.com/B5Mc5y16GVWJZu4tfa8aCXcv",
+  "https://amac-bench-bnb-dev.s3-us-west-1.amazonaws.com/C7fvHRsmaw69Lxd9TBrtRhkx"
 ]
+
+# WALTER_IMGS = [
+#   'app/assets/images/walter_img1.jpg',
+#   'app/assets/images/walter_img2.jpg',
+#   'app/assets/images/walter_img3.jpg',
+#   'app/assets/images/walter_img4.jpg',
+# ]
 
 WALTER_IMGS = [
-  'app/assets/images/walter_img1.jpg',
-  'app/assets/images/walter_img2.jpg',
-  'app/assets/images/walter_img3.jpg',
-  'app/assets/images/walter_img4.jpg',
+  "https://amac-bench-bnb-dev.s3-us-west-1.amazonaws.com/DR9FwpSYFjHvQh9e8W5rofd6",
+  "https://amac-bench-bnb-dev.s3-us-west-1.amazonaws.com/KPoe5xhNTQAQRTpiS44Phwcu",
+  "https://amac-bench-bnb-dev.s3-us-west-1.amazonaws.com/QYSHhJowQZFw1rvJgDin3pqP",
+  "https://amac-bench-bnb-dev.s3-us-west-1.amazonaws.com/VU4jRb1x6FRJ2trXuFFynBP8"
 ]
 
+# COOL_IMGS = [
+#   'app/assets/images/cool1.jpg',
+#   'app/assets/images/cool2.jpg',
+#   'app/assets/images/cool3.jpg',
+#   'app/assets/images/cool4.jpg',
+#   'app/assets/images/cool5.jpg',
+#   'app/assets/images/cool6.jpg',
+# ]
+
 COOL_IMGS = [
-  'app/assets/images/cool1.jpg',
-  'app/assets/images/cool2.jpg',
-  'app/assets/images/cool3.jpg',
-  'app/assets/images/cool4.jpg',
-  'app/assets/images/cool5.jpg',
-  'app/assets/images/cool6.jpg',
+  "https://amac-bench-bnb-dev.s3-us-west-1.amazonaws.com/WU856MySLfBJYvJsW3SXoSjD",
+  "https://amac-bench-bnb-dev.s3-us-west-1.amazonaws.com/ipeWej462ZFiN5eYqRAUT3Km",
+  "https://amac-bench-bnb-dev.s3-us-west-1.amazonaws.com/mjVQmBfcQbDGcLpim9BfDBF7",
+  "https://amac-bench-bnb-dev.s3-us-west-1.amazonaws.com/mjXKWQuHr4AKUJBUpFfMnFtH",
+  "https://amac-bench-bnb-dev.s3-us-west-1.amazonaws.com/qKGYMY9pftkvFTx3JDSQGdxk",
+  "https://amac-bench-bnb-dev.s3-us-west-1.amazonaws.com/wAkFbwLM15z78sVnU9UtUC5h"
 ]
 
 def addPictureToBench(bench_data, picture)
