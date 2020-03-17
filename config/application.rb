609ef62ff595f604c160e91256cc13ad42558d36
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-17 12:21:40 -0700

    placing config command in correct spot

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.serve_static_assets = true
 
 module BenchBnb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
