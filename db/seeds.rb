0746243cc9edc22cad409611c1d18ac6cc5bb3fd
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-17 14:22:11 -0700

    moves images

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,26 +30,26 @@ bench3 = Bench.create!({
 })
 
 POJO_IMGS = [
-  'app/assets/images/pojo_img1.jpg',
-  'app/assets/images/pojo_img2.jpg',
-  'app/assets/images/pojo_img3.jpg',
-  'app/assets/images/pojo_img4.jpg'
+  'images/pojo_img1.jpg',
+  'images/pojo_img2.jpg',
+  'images/pojo_img3.jpg',
+  'images/pojo_img4.jpg'
 ]
 
 WALTER_IMGS = [
-  'app/assets/images/walter_img1.jpg',
-  'app/assets/images/walter_img2.jpg',
-  'app/assets/images/walter_img3.jpg',
-  'app/assets/images/walter_img4.jpg',
+  'images/walter_img1.jpg',
+  'images/walter_img2.jpg',
+  'images/walter_img3.jpg',
+  'images/walter_img4.jpg',
 ]
 
 COOL_IMGS = [
-  'app/assets/images/cool1.jpg',
-  'app/assets/images/cool2.jpg',
-  'app/assets/images/cool3.jpg',
-  'app/assets/images/cool4.jpg',
-  'app/assets/images/cool5.jpg',
-  'app/assets/images/cool6.jpg',
+  'images/cool1.jpg',
+  'images/cool2.jpg',
+  'images/cool3.jpg',
+  'images/cool4.jpg',
+  'images/cool5.jpg',
+  'images/cool6.jpg',
 ]
 
 def addPictureToBench(bench_data, picture)
