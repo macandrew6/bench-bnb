6380bfec4e46534c373edacc35df21ca54789ffa
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-16 21:53:04 -0700

    adding pseudo code for additional review features

--- frontend/components/bench_show/review_index.jsx
+++ frontend/components/bench_show/review_index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReviewIndexItemContainer from './review_index_item_container';
 
 const ReviewIndex = ({ reviews }) => {
+  // set up a show less button for the reviews
   return (
     <div className="reviews-index-container">
       {
