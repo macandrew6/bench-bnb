b7a64a7f0a64f70dc8fb0cf4f255b0c1fa5e9205
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-18 12:25:44 -0700

    removing production env error

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const webpack = require("webpack");
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/bench_bnb.jsx',
+  entry: "./frontend/bench_bnb.jsx",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
   },
   module: {
     rules: [
@@ -13,18 +14,23 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           query: {
-            presets: ['@babel/env', '@babel/react']
+            presets: ["@babel/env", "@babel/react"]
           }
-        },
+        }
       }
     ]
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   resolve: {
     extensions: [".js", ".jsx", "*"]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 };
 
 
