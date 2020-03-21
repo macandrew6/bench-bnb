16388637c6e10a13ffa654d67b0f36c959922e5e
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-20 23:01:11 -0700

    pseudo code plan

--- frontend/components/bench_show/bench_show.jsx
+++ frontend/components/bench_show/bench_show.jsx
@@ -8,6 +8,7 @@ import { ReviewLink } from '../../util/link_util';
 
 const BenchShow = ({ bench, fetchBench, benchId, reviews }) => {
   // slice of state onShowPage:bool
+  // pass down into singleMap remove event listener
   
   const benches = {
     [benchId]: bench
