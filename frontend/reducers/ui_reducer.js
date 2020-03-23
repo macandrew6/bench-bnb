88279afa534e4abf1500d668bd96d8bb4c159b67
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-22 23:44:18 -0700

    pseudo coding loader

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -3,6 +3,8 @@ import filtersReducer from './filters_reducer';
 
 // adding a new slice of state => ref pokedex
 // loading slice of state
+// loading slice of state should be updated via actions 
+  // (action done on receive bench)
 const uiReducer = combineReducers({
   filters: filtersReducer
 });
