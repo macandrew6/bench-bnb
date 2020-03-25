d0012919155981f91e95e85bb3dd077baec3339f
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-24 18:04:34 -0700

    adds loading bench actions

--- frontend/actions/bench_actions.js
+++ frontend/actions/bench_actions.js
@@ -3,6 +3,19 @@ import * as BenchesAPIUtil from '../util/benches_api_util';
 export const RECEIVE_BENCHES = 'RECEIVE_BENCHES';
 export const RECEIVE_BENCH = 'RECEIVE_BENCH';
 export const RECEIVE_REVIEW = 'RECEIVE_REVIEW';
+export const START_LOADING_SINGLE_BENCH = 'START_LOADING_SINGLE_BENCH';
+export const START_LOADING_ALL_BENCHES = 'START_LOADING_ALL_BENCHES';
+
+export const startLoadingAllBenches = () => {
+  return ({
+    type: START_LOADING_ALL_BENCHES
+  });
+};
+export const startLoadingSingleBench = () => {
+  return ({
+    type: START_LOADING_SINGLE_BENCH
+  });
+};
 
 export const receiveBenches = benches => {
   return ({
